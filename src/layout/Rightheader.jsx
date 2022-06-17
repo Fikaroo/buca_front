7ef6b03ef4ea191816196682bb6307ff917bc27c
Fikaroo <fuadbayramv@gmail.com>
2022-06-18 00:14:29 +0400

    finish homepage home section

--- src/layout/Rightheader.jsx
+++ src/layout/Rightheader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 function rightHeader() {
+
   return (
     <div className="flex ml-auto">
       <div className="flex mr-4">
@@ -9,8 +9,8 @@ function rightHeader() {
           id="lang"
           className="flex hover:cursor-pointer w-14 outline-none"
         >
-          <option>AZ</option>
-          <option>ENG</option>
+          <option value="AZ">AZ</option>
+          <option value="ENG">ENG</option>
         </select>
       </div>
       <div className="flex space-x-4">
